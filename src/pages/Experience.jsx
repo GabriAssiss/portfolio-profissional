7098143bb458da4c5761d7e32fe0f867c86c3ed1
Gabriel Assis de Souza <dunkrells23@gmail.com>
2025-08-21 17:37:42 -0300

    style: projects layout

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,15 +1,18 @@
-import SideText from "../components/SideText"
-import PageType from "../components/PageType"
+import SideText from "../components/SideText";
+import PageType from "../components/PageType";
 
 function Experience() {
-    return (
-         <main className="w-screen h-screen">
-      <PageType type="Experiência"/>
-      <section className="flex justify-between items-center h-10/12" >
+  return (
+    <main className="w-screen h-screen">
+      <div className="w-1/3">
+        <PageType type="Experiência" />
+
+      </div>
+      <section className="flex justify-between items-center h-10/12">
         <SideText />
       </section>
-    </main>   
-    )
+    </main>
+  );
 }
 
-export default Experience
+export default Experience;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,26 +1,22 @@
 import PageType from "../components/PageType";
-import SideButton from "../components/SideButton";
-import SideText from "../components/SideText";
-import UpperButton from "../components/UpperButton";
+import ProjectCard from "../components/ProjectCard";
 import { useState } from "react";
 
 function Projects() {
-
   const [showSideText, setShowSideText] = useState(false);
 
   return (
     <main className="w-screen h-screen">
-      <PageType type={"Projetos"}/>
-      <section className="flex justify-between items-center h-10/12">
-        <div>
-          {showSideText && <SideText />}    
-        </div>
-        <section onMouseEnter={() => setShowSideText(true)}>
-          <SideButton />
-          <SideButton />
-          <SideButton />
-          <SideButton />
-        </section>
+      <div className="w-1/3">
+        <PageType type={"Projetos"} />
+      </div>
+      <section className="grid grid-cols-3 h-full w-full place-items-center">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </section>
     </main>
   );
