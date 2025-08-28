e71d1cbbc3d6d6103de187860c9ea0108fa65153
Gabriel Assis de Souza <dunkrells23@gmail.com>
2025-08-28 14:38:41 -0300

    config: vercel configuration update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,10 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
-import dotenv from "dotenv/lib/main";
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const port = process.env.PORT || 3001;
 
 app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
@@ -33,15 +28,11 @@ app.post("/send-email", async (req, res) => {
 
     await transporter.sendMail(mailOptions);
 
-    res
-      .status(200)
-      .json({ success: true, message: "Email enviado com sucesso!" });
+    res.status(200).json({ success: true, message: "Email enviado com sucesso!" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Erro ao enviar email." });
   }
 });
 
-app.listen(port, () => {
-  console.log("Servidor rodando na porta 5000");
-});
+export default app;
